722d3b1d59b04f2090eee2017f01ccf6d834c647
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-04-19 09:55:41 +0100

    feat(Restaurante): Clase Restaurante y atributos agregados

---
+++ src/org/ripadbaisor/hosteleria/Restaurante.java
@@ -0,0 +1,10 @@
+package org.ripadbaisor.hosteleria;
+
+public class Restaurante {
+
+    private String nombre;
+    private String locacion;
+    private String horario;
+    private float puntuacion;
+    
+}
