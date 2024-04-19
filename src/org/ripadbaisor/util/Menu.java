1e3bad931564efc7b3407321b3978d7e3c78fb5b
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-04-19 10:36:45 +0100

    feat(Menu): Método mostarMenuPrincipal agragado a la calse Menu

---
+++ src/org/ripadbaisor/util/Menu.java
@@ -0,0 +1,20 @@
+package org.ripadbaisor.util;
+
+import javax.swing.JOptionPane;
+
+public class Menu {
+
+    public static String mostrarMenuPrincipal() {
+        String opcion = JOptionPane.showInputDialog("""
+                       Elija una opción:
+                    1. Añadir Restaurante
+                    2. Editar Restaurante
+                    3. Mostrar Restaurantes
+                    4. Eliminar Restaurante
+                    Q. Salir del programa
+                """);
+
+        return opcion;
+
+    }
+}
